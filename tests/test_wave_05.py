b07619a010d53641afeaeff24b4d88721416d82e
Rayanna Ferreira <rayanna_ferreira@icloud.com>
2026-09-17 13:23:46 -0700

    complete wave 5 tests

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -54,7 +54,7 @@ def test_new_genre_rec_from_empty_friends():
     }
     recommendations = get_new_rec_by_genre(sonyas_data)
     assert len(recommendations) == 0
-    #raise Exception("Test needs to be completed.")
+    #raise Exception("Test needs to be completed..")
     # *********************************************************************
     # ****** Complete the Act and Assert Portions of these tests **********
     # *********************************************************************
